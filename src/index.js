7a383b395e034b0498e7c1842ca687e9fe73778a
Simon Degraeve <simon.degraeve@gmail.com>
2016-07-06 12:35:00 +0800

    develop: Mirror express-graphql changes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@
  */
 import Joi from 'joi';
 import Boom from 'boom';
-import {Stream} from 'stream';
-import {graphql} from 'graphql';
-import {formatError} from 'graphql/error';
-import {version} from '../package.json';
+import { Stream } from 'stream';
+import {
+  Source,
+  parse,
+  validate,
+  execute,
+  formatError,
+  getOperationAST,
+  specifiedRules,
+} from 'graphql';
+import { version } from '../package.json';
 import renderGraphiQL from './renderGraphiQL';
 import accepts from 'accepts';
+
 
 /**
  * Define constants
@@ -18,24 +26,28 @@ const optionsSchema = {
     Joi.func(),
     Joi.object({
       schema: Joi.object().required(),
+      context: Joi.object(),
       rootValue: Joi.object(),
       pretty: Joi.boolean(),
-      graphiql: Joi.boolean()
-    }).required()
+      graphiql: Joi.boolean(),
+      formatError: Joi.func(),
+      validationRules: Joi.array(),
+    }).required(),
   ],
   route: Joi.object().keys({
     path: Joi.string().required(),
-    config: Joi.object()
-  }).required()
+    config: Joi.object(),
+  }).required(),
 };
 
 
 /**
  * Define helper: get options from object/function
  */
-const getOptions = (options, request) => {
+const getOptions = async (options, request) => {
   // Get options
-  const optionsData = typeof options === 'function' ? options(request) : options;
+  const optionsData = await Promise
+    .resolve(typeof options === 'function' ? options(request) : options);
 
   // Validate options
   const validation = Joi.validate(optionsData, optionsSchema.query);
@@ -54,16 +66,23 @@ const parsePayload = async (request) => {
   const result = await new Promise((resolve) => {
     if (request.payload instanceof Stream) {
       let data = '';
-      request.payload.on('data', chunk => data += chunk);
+      request.payload.on('data', (chunk) => {
+        data += chunk;
+      });
       request.payload.on('end', () => resolve(data));
-    }
-    else {
+    } else {
       resolve('{}');
     }
   });
 
   // Return normalized payload
-  return request.mime === 'application/graphql' ? {query: result} : JSON.parse(result);
+  let formattedResult = null;
+  if (request.mime === 'application/graphql') {
+    formattedResult = { query: result };
+  } else {
+    formattedResult = JSON.parse(result);
+  }
+  return formattedResult;
 };
 
 
@@ -79,8 +98,7 @@ const getGraphQLParams = (request, payload = {}) => {
   if (variables && typeof variables === 'string') {
     try {
       variables = JSON.parse(variables);
-    }
-    catch (error) {
+    } catch (error) {
       throw Boom.badRequest('Variables are invalid JSON.');
     }
   }
@@ -89,16 +107,16 @@ const getGraphQLParams = (request, payload = {}) => {
   const operationName = request.query.operationName || payload.operationName;
 
   // Return params
-  return {query, variables, operationName};
+  return { query, variables, operationName };
 };
 
+
 /**
- * Helper function to determine if GraphiQL can be displayed.
+ * Define helper: determine if GraphiQL can be displayed.
  */
 const canDisplayGraphiQL = (request, data) => {
-
   // If `raw` exists, GraphiQL mode is not enabled.
-  var raw = request.query.raw !== undefined || data.raw !== undefined;
+  const raw = ((request.query.raw !== undefined) || (data.raw !== undefined));
 
   // Allowed to show GraphiQL if not requested as raw and this request
   // prefers HTML over JSON.
@@ -108,30 +126,80 @@ const canDisplayGraphiQL = (request, data) => {
 
 
 /**
- * Define GraphQL runner
+ * Define helper: execute query and create result
  */
-const runGraphQL = async (params, schema, rootValue, showGraphiQL) => {
-
-  const { query, variables, operationName } = params;
-
+const createResult = async ({
+  context,
+  operationName,
+  query,
+  request,
+  rootValue,
+  schema,
+  showGraphiQL,
+  validationRules,
+  variables,
+}) => {
+  // If there is no query, but GraphiQL will be displayed, do not produce
+  // a result, otherwise return a 400: Bad Request.
   if (!query) {
     if (showGraphiQL) {
       return null;
     }
-
     throw Boom.badRequest('Must provide query string.');
   }
 
-  // Run GraphQL query.
-  const result = await graphql(schema, query, rootValue, variables, operationName);
+  // GraphQL source.
+  const source = new Source(query, 'GraphQL request');
 
-  // Format any encountered errors.
-  if (result.errors) {
-    result.errors = result.errors.map(formatError);
+  // Parse source to AST, reporting any syntax error.
+  let documentAST;
+  try {
+    documentAST = parse(source);
+  } catch (syntaxError) {
+    // Return 400: Bad Request if any syntax errors errors exist.
+    throw Boom.badRequest('Syntax error', [syntaxError]);
   }
 
-  // Return GraphQL result
-  return result;
+  // Validate AST, reporting any errors.
+  const validationErrors = validate(schema, documentAST, validationRules);
+  if (validationErrors.length > 0) {
+    // Return 400: Bad Request if any validation errors exist.
+    throw Boom.badRequest('Validation error', validationErrors);
+  }
+
+  // Only query operations are allowed on GET requests.
+  if (request.method === 'get') {
+    // Determine if this GET request will perform a non-query.
+    const operationAST = getOperationAST(documentAST, operationName);
+    if (operationAST && operationAST.operation !== 'query') {
+      // If GraphiQL can be shown, do not perform this query, but
+      // provide it to GraphiQL so that the requester may perform it
+      // themselves if desired.
+      if (showGraphiQL) {
+        return null;
+      }
+
+      // Otherwise, report a 405: Method Not Allowed error.
+      throw Boom.methodNotAllowed(
+        `Can only perform a ${operationAST.operation} operation from a POST request.`
+      );
+    }
+  }
+
+  // Perform the execution, reporting any errors creating the context.
+  try {
+    return await execute(
+      schema,
+      documentAST,
+      rootValue,
+      context,
+      variables,
+      operationName
+    );
+  } catch (contextError) {
+    // Return 400: Bad Request if any execution context errors exist.
+    throw Boom.badRequest('Context error', [contextError]);
+  }
 };
 
 
@@ -139,40 +207,73 @@ const runGraphQL = async (params, schema, rootValue, showGraphiQL) => {
  * Define handler
  */
 const handler = (route, options = {}) => async (request, reply) => {
+  let errorFormatter = formatError;
+
   try {
     // Get GraphQL options given this request.
-    const {schema, rootValue, pretty, graphiql} = getOptions(options, request);
+    const {
+      schema,
+      context,
+      rootValue,
+      pretty,
+      graphiql,
+      formatError: customFormatError,
+      validationRules: additionalValidationRules,
+    } = await getOptions(options, request);
 
-    // Set up JSON output settings
-    if (pretty) {
-      request.route.settings.json.space = 2;
+    let validationRules = specifiedRules;
+    if (additionalValidationRules) {
+      validationRules = validationRules.concat(additionalValidationRules);
+    }
+
+    if (customFormatError) {
+      errorFormatter = customFormatError;
+    }
+
+    // GraphQL HTTP only supports GET and POST methods.
+    if ((request.method !== 'get') && (request.method !== 'post')) {
+      throw Boom.methodNotAllowed('GraphQL only supports GET and POST requests.');
     }
 
     // Parse payload
     const payload = await parsePayload(request);
 
-    // Get GraphQL params from the request and POST body data.
-    const params = await getGraphQLParams(request, payload);
-
     // Can we show graphiQL?
-    const showGraphiQL = graphiql && await canDisplayGraphiQL(request, payload);
+    const showGraphiQL = graphiql && canDisplayGraphiQL(request, payload);
 
-    // Run GraphQL
-    const result = await runGraphQL(params, schema, rootValue, showGraphiQL);
+    // Get GraphQL params from the request and POST body data.
+    const { query, variables, operationName } = getGraphQLParams(request, payload);
 
-    // Show GraphiQL if we can
-    if (showGraphiQL) {
-      const { query, variables } = params;
-      return reply(renderGraphiQL({ query, variables, result }));
+    // Create the result
+    const result = await createResult({
+      context,
+      operationName,
+      query,
+      request,
+      rootValue,
+      schema,
+      showGraphiQL,
+      validationRules,
+      variables,
+    });
+
+    // Format any encountered errors.
+    if (result && result.errors) {
+      result.errors = result.errors.map(errorFormatter);
     }
 
-    // Return result
-    return reply(result)
-      .code(result.hasOwnProperty('data') ? 200 : 400);
-  }
-  catch (error) {
-    // Return error
-    return reply(error);
+    // If allowed to show GraphiQL, present it instead of JSON.
+    if (showGraphiQL) {
+      reply(renderGraphiQL({ query, variables, operationName, result })).type('text/html');
+    } else {
+      // Otherwise, present JSON directly.
+      reply(JSON.stringify(result, null, pretty ? 2 : 0)).type('application/json');
+    }
+  } catch (error) {
+    // Return error, picking up Boom overrides
+    const { statusCode = 500 } = error.output;
+    const errors = error.data || [error];
+    reply({ errors: errors.map(errorFormatter) }).code(statusCode);
   }
 };
 
@@ -184,8 +285,8 @@ handler.defaults = (method) => {
   if (method === 'post') {
     return {
       payload: {
-        output: 'stream'
-      }
+        output: 'stream',
+      },
     };
   }
   return {};
@@ -201,7 +302,7 @@ function register(server, options = {}, next) {
   if (validation.error) {
     throw validation.error;
   }
-  const {route, query} = validation.value;
+  const { route, query } = validation.value;
 
   // Register handler
   server.handler('graphql', handler);
@@ -212,8 +313,8 @@ function register(server, options = {}, next) {
     path: route.path,
     config: route.config,
     handler: {
-      graphql: query
-    }
+      graphql: query,
+    },
   });
 
   // Done
@@ -224,7 +325,7 @@ function register(server, options = {}, next) {
 /**
  * Define plugin attributes
  */
-register.attributes = {name: 'graphql', version};
+register.attributes = { name: 'graphql', version };
 
 
 /**
