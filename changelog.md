74fb59a76a448045c95fa18e16746bb386399c8a
Jason Walsh <jason@walsh-web.com>
2016-11-06 19:09:31 -0700

    Bugfix: StatusCode Runtime Error

--- changelog.md
+++ changelog.md
@@ -1,3 +1,9 @@
+# v1.0.5
+> November 6, 2016
+
+- Fix statusCode runtime error that occurs with GQL objects if there is no error.output
+
+
 # v1.0.4
 
 - Support for GraphQL v0.7.*
--- src/index.js
+++ src/index.js
@@ -271,9 +271,9 @@ const handler = (route, options = {}) => async (request, reply) => {
     }
   } catch (error) {
     // Return error, picking up Boom overrides
-    const { statusCode = 500 } = error.output;
     const errors = error.data || [error];
-    reply({ errors: errors.map(errorFormatter) }).code(statusCode);
+    const statusCode = error.output && error.output.statusCode;
+    reply({ errors: errors.map(errorFormatter) }).code(statusCode || 500);
   }
 };
 
